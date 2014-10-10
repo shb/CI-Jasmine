6855351ebf80d9513d52856c9a554f0b1b04ce26
Simone Cociancich <simone.coch@tin.it>
2014-10-10 17:48:48 +0200

    Thrown in correct handling of success, failures, and exceptions

--- application/libraries/CI_Jasmine.php
+++ application/libraries/CI_Jasmine.php
@@ -34,6 +34,9 @@ class Suite
 
 	public function run()
 	{
+		$app = get_instance();
+		$app->load->library('unit_test');
+
 		// Set the current suite instance
 		\Jasmine::suite($this);
 		// Run each defined specification inside this suite instance
@@ -42,57 +45,93 @@ class Suite
 			// Set the current specification
 			$this->spec = $spec;
 			$ctx = new \stdClass;
-			// Possibly run setup handler
-			if (isset($this->setup)) {
-				$setup = $this->setup->bindTo($ctx);
-				$setup();
+			try
+			{
+				// Possibly run setup handler
+				if (isset($this->setup)) {
+					$setup = $this->setup->bindTo($ctx);
+					$setup();
+				}
+				// Try to run the expectation test for the current specification
+				$boundTest = $test->bindTo($ctx);
+				try {
+					$boundTest();
+					echo $app->unit->run(TRUE,TRUE, $this->topic());
+				} catch (Failure $fail) {
+					// Print failure message and continue to next test
+					echo $app->unit->run(FALSE,TRUE, $this->topic(), "Test failed: ".$fail->getMessage());
+					continue;
+				}
+				//TODO: posisbily run teardown handler
 			}
-			// Run the expectation test for the current specification
-			$boundTest = $test->bindTo($ctx);
-			$boundTest();
+			catch (\Exception $ex)
+			{
+				// Print exception and halt testing
+				echo $app->unit->run(FALSE,TRUE, $this->topic(), "Exception thrown: ".$ex->getMessage());
+				break;
+			}
 		}
 	}
 }
 
 class Expectation
 {
-	private $unit;
+	//private $unit;
 	private $suite;
 	private $actual;
 	private $positivity = TRUE;
+	private $be = 'is not';
 
 	public function __construct ($actual, $suite=NULL, $positivity=TRUE)
 	{
-		$CI = get_instance();
+		/*$CI = get_instance();
 		$CI->load->library('unit_test');
-		$this->unit = $CI->unit;
+		$this->unit = $CI->unit;*/
 
 		$this->suite = isset($suite)? $suite : \Jasmine::suite();
 		$this->actual = $actual;
 		$this->positivity = $positivity;
 		
-		if ($positivity)
+		if ($positivity) {
+			$this->be = 'is not';
 			$this->not = new \CI_Jasmine\Expectation($actual, $suite, FALSE);
+		} else {
+			$this->be = 'is indeed';
+		}
+	}
+	
+	private function fail ($verb, $expected='')
+	{
+		$msg = trim("{$this->actual} {$this->be} {$verb} {$expected}");
+		throw new Failure($msg);
 	}
 
 	public function toEqual ($expected)
 	{
-		echo $this->unit->run($this->actual == $expected, $this->positivity, $this->suite->topic());
+		if (($this->actual == $expected) === $this->positivity) return TRUE;
+		else $this->fail('equal to', $expected);
 	}
 
 	public function toBe($expected)
 	{
-		echo $this->unit->run($this->actual === $expected, $this->positivity, $this->suite->topic());
+		if (($this->actual === $expected) === $this->positivity) return TRUE;
+		else $this->fail('the same as', $expected);
 	}
 	
 	public function toBeDefined()
 	{
-		echo $this->unit->run(isset($this->actual), $this->positivity, $this->suite->topic());
+		if (isset($this->actual) === $this->positivity) return TRUE;
+		else $this->fail('defined');
 	}
 	
 	public function toBeUndefined()
 	{
-		echo $this->unit->run(!isset($this->actual), $this->positivity, $this->suite->topic());
+		if (!isset($this->actual) === $this->positivity) return TRUE;
+		else $this->fail('undefined');
 	}
 
+}
+
+class Failure extends \Exception
+{
 }
